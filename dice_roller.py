4c9a841234ef07cb14d793810b163cb0de262de4
xRin123 <86306405+xRin123@users.noreply.github.com>
2021-06-25 18:10:40 +0800

    Add files via upload

--- dice_roller.py
+++ dice_roller.py
@@ -1,11 +1,18 @@
 import random
 def main():
-  dice_rolls = 2
+  dice_rolls = int ( input ( '你想掷多少个骰子？' ))
+  dice_size = int(input('骰子有几面？'))
   dice_sum = 0
   for i in range(0, dice_rolls):
-    roll = random.randint(1,6)
+    roll = random.randint(1, dice_size)
     dice_sum += roll
-    print( f'You haveroll a total of { dice_sum } ')
+    if roll == 1:
+      print(f'You roll a {roll} !Critical Fail')
+    elif roll == dice_size :
+      print(f'You roll a {roll}！关键的成功！')
+    else:
+      print(f'You roll a {roll} ')
+  print(f'你总共掷了{dice_sum} ')
 
 if __name__== "__main__":
   main()
